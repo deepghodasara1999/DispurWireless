aef0df805796aeae0d6627e2814dec1a7299ddfb
Deep <deepghodasara1999@gmail.com>
2020-10-25 13:31:57 +0530

    test

--- main.py
+++ main.py
@@ -1,28 +1,5 @@
-import pymysql
-pymysql.install_as_MySQLdb()
-
-from flask import Flask, render_template,request
-from flask_sqlalchemy import SQLAlchemy
-
+from flask import Flask, render_template
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:@localhost/case_study'
-db = SQLAlchemy(app)
-
-class Registration(db.Model):
-	c_id = db.Column(db.Integer, unique=True, primary_key=True)
-	fname = db.Column(db.String, nullable=False)
-	lname = db.Column(db.String, nullable=False)
-	contact = db.Column(db.String, nullable=False)
-	email = db.Column(db.String, nullable=False)
-	pwd = db.Column(db.String, nullable=False)
-
-	def __init__(self,c_id,fname,lname,contact,email,pwd):
-		self.c_id = c_id
-		self.fname = fname
-		self.lname = lname
-		self.contact = contact
-		self.email = email
-		self.pwd = pwd
 
 @app.route("/")
 def home():
@@ -30,28 +7,14 @@ def home():
 
 @app.route("/login")
 def login():
-	if(request.method == "POST"):
-		pass
-	else:
-		return render_template('login.html')
+	return render_template('login.html')
 
-@app.route("/register", methods=['GET', 'POST'])
+@app.route("/about")
+def about():
+	return render_template('about.html')
+
+@app.route("/register")
 def register():
-	if(request.method == "POST"):
-		c_id = 12345
-		fname = request.form.get('fname')
-		lname = request.form.get('lname')
-		contact = request.form.get('contact')
-		email = request.form.get('email')
-		pwd = request.form.get('pwd')
-
-		usr = Registration(c_id,fname,lname,contact,email,pwd)
-		db.session.add(usr)
-		db.session.commit()
-
-		return render_template('login.html')
-
-	else:
-		return render_template('register.html')
+	return render_template('register.html')
 
 app.run(debug=True)
