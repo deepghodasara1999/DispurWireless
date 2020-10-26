dcd54f132fc3e7710b6a771244ac1879feed9efc
jeetthummar <jeetthummar143@gmail.com>
2020-10-26 13:56:04 +0530

    trying to complete mail

--- main.py
+++ main.py
@@ -3,10 +3,24 @@ pymysql.install_as_MySQLdb()
 
 from flask import Flask, render_template,request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
+import math
+import random
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:@localhost/case_study'
 db = SQLAlchemy(app)
+
+app = Flask(__name__)
+app.config.update(
+    MAIL_SERVER='smtp.gmail.com',
+    MAIL_PORT='465',
+    MAIL_USE_SSL=True,
+    MAIL_USERNAME='dispurwireless@gmail.com',
+    MAIL_PASSWORD='Dispur@2020',
+    MAIL_DEFAULT_SENDER='dispurwireless@gmail.com'
+)
+mail = Mail(app)
 
 class Registration(db.Model):
 	c_id = db.Column(db.Integer, unique=True, primary_key=True)
@@ -30,6 +44,10 @@ class Registration(db.Model):
 class CurrentUser:
 	usrObj =None
 
+class Verify():
+    customerObj = None
+    CustomerOTP = None
+
 @app.route("/")
 def home():
 	return render_template('index.html')
@@ -51,18 +69,51 @@ def login():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
-	if(request.method == "POST"):
-		fname = request.form.get('fname')
-		lname = request.form.get('lname')
-		contact = request.form.get('contact')
-		email = request.form.get('email')
-		pwd = request.form.get('pwd')
+	# if(request.method == "POST"):
+	# 	fname = request.form.get('fname')
+	# 	lname = request.form.get('lname')
+	# 	contact = request.form.get('contact')
+	# 	email = request.form.get('email')
+	# 	pwd = request.form.get('pwd')
+	#
+	# 	usr = Registration(fname,lname,contact,email,pwd)
+	# 	db.session.add(usr)
+	# 	db.session.commit()
+	#
+	# 	return render_template('login.html')
+	# else:
+	# 	return render_template('register.html')
 
-		usr = Registration(fname,lname,contact,email,pwd)
-		db.session.add(usr)
-		db.session.commit()
+	if (request.method == "POST"):
+		if request.form["btn"] == "register":
+			fname = request.form.get('fname')
+			lname = request.form.get('lname')
+			contact = request.form.get('contact')
+			email = request.form.get('email')
+			pwd = request.form.get('pwd')
+			customerObj = Registration(fname,lname,contact,email,pwd)
+			Verify.customerObj = customerObj
 
-		return render_template('login.html')
+			OTP = math.floor(random.random() * 1000000)
+			mail.send_message('Email Verification',
+							  recipients=[email, ],
+							  body="Your OTP is : " + str(OTP)
+							  )
+			Verify.CustomerOTP = OTP
+			##--> code for invalid id
+
+			return render_template('verify.html')
+
+		else:
+			if request.form.get('otp') == str(Verify.CustomerOTP):
+				db.session.add(Verify.customerObj)
+				db.session.commit()
+				##--> code for send coustomer Id
+				return render_template('confirm.html')
+			else:
+				##--> code here for wrong OTP
+				return redirect("/register")
+
 	else:
 		return render_template('register.html')
 
